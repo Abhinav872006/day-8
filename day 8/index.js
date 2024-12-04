0ebcb0e28eae2ee9139085c11200f06ce31756e0
Abhinav872006 <abhinavj9852@gmail.com>
2024-12-04 15:56:40 +0530

    day 8

---
+++ day 8/index.js
@@ -0,0 +1,81 @@
+// // console.log("----welcome in wolrd of Tecnology----");
+
+// // let arr =[1,2,3,4,5,6,7,"Hello","True"]
+
+// // for( let i=0 ; i < arr.length; i++){
+
+// //     console.log(arr[i]);
+// // }
+
+// // arr.push("abhinav jha");
+// // console.log("............");
+// // arr.pop();
+// // arr.splice(2,1);    
+
+// // for(let i = 0;i<arr.length;i++){
+// //     console.log(arr[i])
+// // }
+
+// const Newarr = [5,2,6,4,3,5,4,56,4,3]
+
+// let sum = 0;
+
+// for(let i = 0; i < Newarr.length;i++){
+    
+
+//     if(Newarr[i] % 2 !== 0){
+
+//     sum = sum + Newarr[i];
+//     }
+
+// }
+// console.log(sum);
+
+
+
+// // let arr = [1,4,5,6,73,2,4]
+// // arr.splice(5,1)
+
+// // for(let i = 0;i<arr.length;i++){
+// //     console.log(arr[i]);
+// // }
+
+
+let student_id_card = {
+    name:"abhinav",
+    rollnumber:244016,
+    address:"adc....",
+    section: "C",
+    course: "B-TECH",
+
+};
+
+const address = student_id_card.address;
+const course = student_id_card.course;
+const  rollnumber = student_id_card.rollnumber;
+const section = student_id_card.section;
+const name = student_id_card.name;
+
+
+student_id_card.Result  = "pass";
+
+
+console.log(address,course,rollnumber,section, name );
+
+student_id_card.Result  = "pass";
+
+student_id_card.ID  = "8851352248";
+
+const ID = student_id_card.ID;
+const Result = student_id_card.Result;
+
+delete student_id_card.name
+
+
+console.log(Result);
+console.log(ID);
+
+
+
+
+
